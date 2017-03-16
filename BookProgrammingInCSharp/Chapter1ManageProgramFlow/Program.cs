a3d0f776f101a7726e97ceb1c1c69222296a9643
Gustavo Aquino <gustavo.aquino@outlook.com>
2017-03-15 23:46:51 -0300

    1) Adicionado exemplos de uso de Threads. 2) Threads parametrizadas.

--- BookProgrammingInCSharp/Chapter1ManageProgramFlow/Program.cs
+++ BookProgrammingInCSharp/Chapter1ManageProgramFlow/Program.cs
@@ -1,15 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chapter1ManageProgramFlow
+﻿namespace Ch1Threads01
 {
+    using System.Threading;
+    using static System.Console;
+    using static System.Threading.Thread;
+
     class Program
     {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                WriteLine($"ThreadProc: {i}");
+                Sleep(1000);
+            }
+        }
+
+        public static void ThreadMethodComParametro(object obj)
+        {
+            for (int i = 0; i < (int) obj; i++)
+            {
+                WriteLine($"ThreadProc: {i}");
+                Sleep(0);
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Instancia uma nova Thread para executar o método definido.
+            var thread = new Thread(new ThreadStart(ThreadMethod));
+
+            thread.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                WriteLine("Método principal");
+                Sleep(0);
+            }
+
+            // Aguarda a Worker Thread invocada terminar a execução para prosseguir.
+            thread.Join();
+
+            ReadKey();
+
+            // Com Background = false, essa nova Thread será executada sobre a Main Thread que já estará finalizada.
+            // ParameterizedThreadStart = Permite que um parâmetro seja passado para o método da Thread invocada.
+            var outraThread = new Thread(new ParameterizedThreadStart(ThreadMethodComParametro))
+            {
+                IsBackground = false
+            };
+
+            // Envio de parâmetro (contador para o loop na Thread).
+            outraThread.Start(3);
+
+            ReadKey();
         }
     }
 }
