765a53ce2a9d360490a4c49816c5a13630de3a49
Gustavo Aquino <gustavo.aquino@outlook.com>
2017-03-30 01:09:08 -0300

    Adicionado exemplo de Task simples e Task com retorno de valor.

--- BookProgrammingInCSharp/Ch1.1-Tasks01/Program.cs
+++ BookProgrammingInCSharp/Ch1.1-Tasks01/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ch1._1_Tasks01
+﻿namespace Ch1._1_Tasks01
 {
+    using System.Threading.Tasks;
+    using static System.Console;
+
     class Program
     {
         static void Main(string[] args)
         {
+            goto Inicio;
+
+            // Criando uma nova Task e iniciando ela imediatamente.
+            // Pg 11.
+            var task = Task.Run(() =>
+            {
+                for (int x = 0; x < 100; x++)
+                {
+                    WriteLine(x);
+                }
+            });
+
+            // Equivalente a Join na Thread que esperará com que a Task finalize para prosseguir
+            // na execução das linhas abaixo.
+            task.Wait();
+
+            Inicio:
+
+            // Task que retorna valor.
+            // Pg 12.
+            Task<string> taskRetornaValor = Task.Run(() =>
+            {
+                return "Olá";
+            });
+
+            // Printa o resultado da Task executada.
+            WriteLine(taskRetornaValor.Result);
+
+            ReadLine();
         }
     }
 }
